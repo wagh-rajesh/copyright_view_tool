960ddb46d8235c08c483fd18df4543969e99776c
wraj <waghrajesh6@gmail.com>
2017-09-03 21:25:03 +0530

    Copyright view tool initial commit

---
+++ lib/copyright_view_tool.rb
@@ -0,0 +1,6 @@
+require "copyright_view_tool/version"
+require "copyright_view_tool/renderer"
+
+module CopyrightViewTool
+  # Your code goes here...
+end
---
+++ lib/copyright_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CopyrightViewTool
+  VERSION = "0.1.0"
+end
